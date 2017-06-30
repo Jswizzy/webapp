20adfa5373737d495bc21800d219239ec7525208
jswizzy <smithjustinn@hotmail.com>
2017-06-29 23:47:59 -0400

    add template

--- src/main/main.go
+++ src/main/main.go
@@ -1,9 +1,52 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 )
 
 func main() {
-	http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Add("Content-Type", "text/html")
+		templates := template.New("template")
+		templates.New("test").Parse(doc)
+		templates.New("header").Parse(header)
+		templates.New("footer").Parse(footer)
+		context := Context{
+			[3]string{"Lemon", "Orange", "Apple"},
+			"the title",
+		}
+		templates.Lookup("test").Execute(writer, context)
+
+	})
+
+	http.ListenAndServe(":8000", nil)
+	//http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
+}
+
+const doc = `
+{{template "header" .Title}}
+	<body>
+		<h1>List of Fruit</h1>
+		<ul>
+			{{range .Fruit}}
+				<li>{{.}}</li>
+			{{end}}
+		</ul>
+	</body>
+{{template "footer"}}
+`
+const header = `
+<!DOCTYPE html>
+<html>
+	<head><title>{{.}}</title></head>
+`
+
+const footer = `
+</html>
+`
+
+type Context struct {
+	Fruit [3]string
+	Title string
 }
