4ef7e1cbf73c160dc3f3ff92bb405e9f4eb9e1f8
jswizzy <smithjustinn@hotmail.com>
2017-07-01 02:14:30 -0400

    add view

--- src/main/main.go
+++ src/main/main.go
@@ -1,52 +1,87 @@
 package main
 
 import (
+	"bufio"
 	"html/template"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"viewmodels"
 )
 
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Header().Add("Content-Type", "text/html")
-		templates := template.New("template")
-		templates.New("test").Parse(doc)
-		templates.New("header").Parse(header)
-		templates.New("footer").Parse(footer)
-		context := Context{
-			[3]string{"Lemon", "Orange", "Apple"},
-			"the title",
-		}
-		templates.Lookup("test").Execute(writer, context)
+	templates := populateTemplates()
 
-	})
+	http.HandleFunc("/",
+		func(writer http.ResponseWriter, request *http.Request) {
+			requestedFile := request.URL.Path[1:]
+			template := templates.Lookup(requestedFile + ".html")
+
+			var context interface{} = nil
+			switch requestedFile {
+			case "home":
+				context = viewmodels.GetHome()
+			case "categories":
+				context = viewmodels.GetCategories()
+			case "products":
+				context = viewmodels.GetProducts()
+			case "product":
+				context = viewmodels.GetProduct()
+			}
+			if template != nil {
+				template.Execute(writer, context)
+			} else {
+				writer.WriteHeader(404)
+			}
+		})
+
+	http.HandleFunc("/img/", serveResource)
+	http.HandleFunc("/css/", serveResource)
 
 	http.ListenAndServe(":8000", nil)
-	//http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
 }
 
-const doc = `
-{{template "header" .Title}}
-	<body>
-		<h1>List of Fruit</h1>
-		<ul>
-			{{range .Fruit}}
-				<li>{{.}}</li>
-			{{end}}
-		</ul>
-	</body>
-{{template "footer"}}
-`
-const header = `
-<!DOCTYPE html>
-<html>
-	<head><title>{{.}}</title></head>
-`
+func serveResource(writer http.ResponseWriter, request *http.Request) {
+	path := filepath.Join("public", request.URL.Path)
+	var contentType string
+	if strings.HasSuffix(path, ".css") {
+		contentType = "text/css"
+	} else if strings.HasSuffix(path, ".png") {
+		contentType = "image/png"
+	} else {
+		contentType = "text/plain"
+	}
 
-const footer = `
-</html>
-`
+	file, err := os.Open(path)
 
-type Context struct {
-	Fruit [3]string
-	Title string
+	if err == nil {
+		defer file.Close()
+		writer.Header().Add("Content-Type", contentType)
+		bufferReader := bufio.NewReader(file)
+		bufferReader.WriteTo(writer)
+	} else {
+		writer.WriteHeader(404)
+	}
+}
+
+func populateTemplates() *template.Template {
+	result := template.New("templates")
+
+	basePath := "templates"
+	templateFolder, _ := os.Open(basePath)
+	defer templateFolder.Close()
+
+	templatePathsRaw, _ := templateFolder.Readdir(-1)
+	templatePaths := new([]string)
+	for _, pathInfo := range templatePathsRaw {
+		if !pathInfo.IsDir() {
+			*templatePaths = append(*templatePaths,
+				filepath.Join(basePath, pathInfo.Name()))
+		}
+	}
+
+	result.ParseFiles(*templatePaths...)
+
+	return result
 }
